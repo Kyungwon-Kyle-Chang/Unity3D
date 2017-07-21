5f82913a8229312e66945848a0dffacbf177fdd0
KyungwonChang <kwmnusa@naver.com>
2017-07-21 10:47:12 +0900

    Paging Scroll View Test

---
+++ PagingScrollView/Assets/ViewController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class ViewController : MonoBehaviour {
+
+    private RectTransform cachedRectTransform;
+    public RectTransform CachedRectTransform
+    {
+        get
+        {
+            if (cachedRectTransform == null)
+                cachedRectTransform = GetComponent<RectTransform>();
+            return cachedRectTransform;
+        }
+    }
+
+    public virtual string Title { get { return ""; } set { } }
+}
