0308c5126b1a66bf2e5a719414cd8a451f1a379a
KyungwonChang <kwmnusa@naver.com>
2017-07-21 10:52:56 +0900

    Just For Test

---
+++ Test/Assets/Draw.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Draw : MonoBehaviour, IUpper {
+
+    void IUpper.Draw()
+    {
+        StartCoroutine(ITest());
+    }
+
+    IEnumerator ITest()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("a");
+    }
+
+}
---
+++ Test/Assets/Manager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject dd;
+
+	void Start () {
+        IUpper test = new Draw();
+        test.Draw();
+	}
+
+}
